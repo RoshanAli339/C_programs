e30e71436962ccce81f29861964f403bf306a542
RoshanAli339 <69452049+RoshanAli339@users.noreply.github.com>
2021-12-21 16:00:25 +0000

    Add files via upload

---
+++ revs.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+  char s[100];
+  printf("Enter a string: ");
+  fgets(s,sizeof(s),stdin);
+  int l=0;
+  char rev[l+1];
+  while(s[l]!='\0')
+    ++l;
+  for(int i=l;i>=0;--i)
+    printf("%c",s[i]);
+  printf("\n");
+}
