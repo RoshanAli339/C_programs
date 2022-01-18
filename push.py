75d4ddd5648da3a6242ffb5d2f50177ab74c2f48
RoshanAli339 <roshanali339lps@gmail.com>
2022-01-18 21:47:27 +0530

    Tuesday 18 January 2022 21:47:27

--- push.py
+++ push.py
@@ -30,7 +30,7 @@ def main():
     command = "git add " + add
     run_command(command)
 
-    commit = commit.replace(" ","")
+    commit = commit.replace(" ",chr(32))
     command = 'git commit -m "' + commit + '"'
     run_command(command)
 
