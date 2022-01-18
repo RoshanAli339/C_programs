cf192912c9e861a50970d2ad48b87b56f0a37cfa
RoshanAli339 <roshanali339lps@gmail.com>
2022-01-18 21:26:11 +0530

    "Tuesday''18''January''2022''21:26:11"

--- push.py
+++ push.py
@@ -30,6 +30,7 @@ def main():
     command = "git add " + add
     run_command(command)
 
+    commit = commit.replace(" ","''")
     command = 'git commit -m "' + commit + '"'
     run_command(command)
 
