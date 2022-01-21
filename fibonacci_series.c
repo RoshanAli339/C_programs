b4061a10e4fd3d1b9a9ec9f65f9f4618fcce10b6
RoshanAli339 <roshanali339lps@gmail.com>
2022-01-21 20:53:15 +0530

    Friday 21 January 2022 08:53:15 PM IST

---
+++ fibonacci_series.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 0, b = 1, c, n;
+	printf("Enter the number of terms: ");
+	scanf("%d",&n);
+	
+	printf("\fThe fibonacci numbers are: %d %d ", a, b); 
+	for (int i = 2; i < n; ++i) 
+	{
+		c = a + b;
+		printf("%d ",c);
+		a = b;
+		b = c;
+	}
+}
