30a8ae750894ebf54da3805b4d0db786dfa6c514
RoshanAli339 <roshanali339lps@gmail.com>
2022-01-21 19:14:27 +0530

    Friday 21 January 2022 07:14:21 PM IST

--- pascals_triangle.c
+++ pascals_triangle.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 
-int main(void)
+int factorial(int n);
+
+int main()
 {
-	printf(("Goto College\n");
-	printf("I am Roshan\n");
+	int ncr;
+
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < 5; ++j)
+		{	
+			if(j <= i)
+			{
+				ncr = factorial(i) / (factorial(j) * factorial(i - j));
+				printf("%d ",ncr);
+			}
+		}
+		printf("\n");
+	}
+
+	for (int i = 3; i >= 0; --i)
+	{
+		for (int j = 3; j >= 0; --j)
+		{
+			if (j <= i)
+			{
+				ncr = factorial(i) / (factorial(j) * factorial(i - j));
+				printf("%d ",ncr);
+			}
+		}
+		printf("\n");
+	}
 }
+
+int factorial(int n)
+{
+	if (n == 1 || n == 0)
+	{
+		return 1;
+	}
+	return n * factorial(n - 1);
+}
+
