a69fa6d4a72c4f855a792a756770f15ec7e2b0c9
RoshanAli339 <roshanali339lps@gmail.com>
2022-02-14 21:59:42 +0530

    Monday 14 February 2022 09:59:42 PM IST

--- hello.c
+++ hello.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-	int a,b;
-	printf("Enter two numbers: ");
-	scanf("%d+%d",&a,&b);
-	printf("Sum = %d",a+b);
+	printf("Hello world!\n");
 }
---
+++ inserting_arr.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void insert(int[], int, int, int);
+
+int main()
+{
+	int arr[20], n;
+	printf("Enter the number of elements: ");
+	scanf("%d", &n);
+
+	printf("Enter the number of elements: ");
+	for(int i = 0; i < n; ++i)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	int x, pos;
+	printf("Enter the element you want to insert: ");
+	scanf("%d", &x);
+
+	printf("Enter the position where you want to insert the given element: ");
+	scanf("%d", &pos);
+
+	insert(arr, n, x, pos);
+	
+	printf("After inserting: ");
+	for (int i = 0; i < n; ++i)
+	{
+		printf("%d ", arr[i]);
+	}
+        
+	return 0;
+}
+
+void insert(int a[20], int n, int x, int pos)
+{
+	int i;
+	for (i = n ; i < pos - 1; ++i)
+	{
+		a[i] = a[i  - 1];
+	}
+
+	a[pos - 1] = x;
+}
