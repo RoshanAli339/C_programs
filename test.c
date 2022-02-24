58cf913788189f016d74d33f75a862452034f6ad
RoshanAli339 <roshanali339lps@gmail.com>
2022-02-24 14:56:52 +0530

    Thursday 24 February 2022 02:56:52 PM IST

--- test.c
+++ test.c
@@ -4,5 +4,6 @@ int main()
 {
 	int b = 5, j;
 	j = (++b) * (++b) * (b++);
+  printf("H");
 	printf("%d %d\n", j, b);
 }
---
+++ tet.py
@@ -0,0 +1,4 @@
+n = int(input("Enter n: "))
+print("Hello world")
+print("Your age is ", n)
+print("Bye")
